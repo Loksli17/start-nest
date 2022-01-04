8e39453f7dc1bfb09d2966443d2e66d93238c4b9
Loksli17 <ami0504@mail.ru>
2022-01-04 18:22:24 +1000

    db config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,13 @@
 export default {
     app: {
         port: 3000,
-    }
+    },
+
+     db: {
+        name    : 'todolist',
+        user    : 'root',
+        password: '1234',
+        port    : 3306,
+        host    : '127.0.0.1',
+    },
 }
