4adcffaad79cc1502a0c029d298827cd931877d5
Loksli17 <ami0504@mail.ru>
2022-02-13 22:11:13 +1000

    todo interface full

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,7 +6,7 @@
         </div>
 
         <div class=" mt-6">
-            <button class="p-4 w-max bg-blue-500  text-white text-lg" @click="newTask">New task</button>
+            <button class="p-4 w-max bg-blue-500  text-white text-lg" @click="showNewModal()">New task</button>
         </div>
 
         <div class=" mt-14 grid gap-5 md:grid-cols-1">
@@ -17,31 +17,33 @@
                 <span>{{task.taskTypeId}}</span>
 
                 <div class="grid grid-flow-col auto-cols-max gap-3">
-                    <button>&#9998;</button>
+                    <button @click="showEditModal(task.id)">&#9998;</button>
                     <button class=" text-red-500" @click="removeTask(task.id)">&#10006;</button>
                 </div>
             </div>
         </div>
     </div>
 
-<!-- 
-    <div class="modal-back">
 
-        <form>
-            <input type="text" name="text" v-model="text" id="">
+    <div @click.self="modalToggle = false" class="grid justify-center items-center fixed w-full h-full z-1 bg-gray-800 opacity-90 top-0 left-0" :class="{'hidden': !modalToggle}">
+
+        <form class=" bg-white p-8 grid gap-10 opacity-100">
+            <input class="cursor-pointer border-2 border-gray-600 p-3" type="text" name="text" v-model="text" id="">
             
-            <select name="taskTypeId">
+            <select class="cursor-pointer border-2 border-gray-600 p-3" v-model="typeId" name="taskTypeId">
                 <option v-for="type in types" :key="type.id" :value="type.id">{{type.name}}</option>
             </select>
+
+            <input class=" p-4 bg-blue-500 text-white cursor-pointer" type="submit" @click.prevent="submitClick" value="Send task">
         </form>
-    </div> -->
+    </div>
     
 </template>
 
 
 
 <script lang="ts">
-    import { defineComponent, ref, Ref } from 'vue';
+    import { defineComponent, ref, Ref, watch } from 'vue';
     import axios, { AxiosResponse }      from 'axios';
 
 
@@ -50,9 +52,20 @@
 
         setup(){
 
-            const 
+            let
+                actionStatus: Ref<string>                     = ref('add'),
+                modalToggle : Ref<boolean>                    = ref(false),
+                id          : Ref<number>                     = ref(1),
+                text        : Ref<string>                     = ref('Text..'),
+                typeId      : Ref<number>                     = ref(1),
+                types       : Ref<Array<Record<string, any>>> = ref([]),
+                tasks       : Ref<Array<Record<string, any>>> = ref([]);
+
+
+            const
+            
                 newTask = () => {
-                    axios.put('http://localhost:3000/task/add', {task: {text: 'Teeeext', taskTypeId: 1}}).then((response: AxiosResponse) => {
+                    axios.put('http://localhost:3000/task/add', {task: {text: text.value, taskTypeId: typeId.value}}).then((response: AxiosResponse) => {
                         console.log(response);
                         return response
                     }).then(() => {
@@ -75,21 +88,50 @@
                     });
                 },
 
+                editTask = (id: number) => {
+                    axios.put(`http://localhost:3000/task/edit/${id}`, {task: {text: text.value, taskTypeId: typeId.value}}).then((response: AxiosResponse) => {
+                        console.log(response);
+                    })
+                },
+
                 getTypes = () => {
                     axios.get(`http://localhost:3000/task-type/get-all`).then((response: AxiosResponse) => {
                         types.value = response.data.types;
                         console.log(types.value);
                     });
+                },
+
+
+                showEditModal = (id_: number) => {
+                    modalToggle.value  = true;
+                    actionStatus.value = 'edit';
+                    id.value           = id_;
+                },
+
+                showNewModal = () => {
+                    modalToggle.value = true;
+                    actionStatus.value = 'add';
+                },
+
+                submitClick = () => {
+                    switch(actionStatus.value){
+                        case 'add': 
+                            newTask();
+                            break;
+                        case 'edit':
+                            editTask(id.value);
+                            break;
+                    }
+                    modalToggle.value = false;
                 };
 
-            let 
-                id   : Ref<number>                     = ref(1),
-                text : Ref<string>                     = ref(''),
-                types: Ref<Array<Record<string, any>>> = ref([]),
-                tasks: Ref<Array<Record<string, any>>> = ref([]);
 
             getTasks();
             getTypes();
+
+            watch(modalToggle, (value: boolean) => {
+                document.body.style.overflowY = value ? 'hidden' : 'auto'; 
+            });
 
             return {
                 tasks,
@@ -98,8 +140,18 @@
                 newTask,
                 getTasks,
                 removeTask,
+                editTask,
+
+                actionStatus,
                 text,
-                id
+                typeId,
+                id,
+
+                modalToggle,
+
+                showEditModal,
+                showNewModal,
+                submitClick
             };
         }
     });
