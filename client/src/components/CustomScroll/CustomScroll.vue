936c7503f955768e9cfa24ac2c95c23ce71cbc84
Loksli17 <ami0504@mail.ru>
2022-02-15 20:07:55 +1000

    customScroll progress for body

--- client/src/components/CustomScroll/CustomScroll.vue
+++ client/src/components/CustomScroll/CustomScroll.vue
@@ -1,14 +1,15 @@
 <template>
 
-    <div ref="wrapper">
+    <div ref="wrapperHtmlRef" class="scroll-main-wrapper">
         <slot></slot>
 
-        <div ref="scrollerWrap" class="scroll-wrap">
-            <div ref="scroller" class="scroller"></div>
+        <div ref="scrollerWrapHtmlRef" :class="{'scroll-wrap-body': body}">
+            <div ref="scrollerHtmlRef" :class="{'scroller-body': body}"></div>
         </div>
     </div>
 
 </template>
+
 
 
 <script lang="ts">
@@ -26,67 +27,147 @@
         setup(props, { emit }){
 
             let
-                oldYVal     : Ref<number> = ref(0),
-                scrollerWrap              = ref(null),
-                scroller                  = ref(null),
-                docHeight   : Ref<number> = ref(0),
-                screenHeight: Ref<number> = ref(0),
-                percent     : Ref<number> = ref(0);
+                scrollerHeight: Ref<number> = ref(0), //* in percent
+                wrapperHeight : Ref<number> = ref(0),
+                screenHeight  : Ref<number> = ref(0);
+
+
+            const
+                wrapperHtmlRef     : Ref<any> = ref(null),
+                scrollerWrapHtmlRef: Ref<any> = ref(null),
+                scrollerHtmlRef    : Ref<any> = ref(null);
 
             let
-                scrollWrap: HTMLDivElement,
-                scrollElem: HTMLDivElement;
+                wrapperHtml     : HTMLDivElement,
+                scrollerWrapHtml: HTMLDivElement,
+                scrollerHtml    : HTMLDivElement;
 
-            if(props.body){
-                screenHeight.value = document.documentElement.clientHeight;
-            } else {
-                // screenHeight.value = document
-            }
 
-            const countData = () => {
-                docHeight.value = Math.max(
-                    document.body.scrollHeight, document.documentElement.scrollHeight,
-                    document.body.offsetHeight, document.documentElement.offsetHeight,
-                    document.body.clientHeight, document.documentElement.clientHeight
-                );
+            const countWrapperHeight = () => {
+                if(props.body){
+                    wrapperHeight.value = Math.max(
+                        document.body.scrollHeight, document.documentElement.scrollHeight,
+                        document.body.offsetHeight, document.documentElement.offsetHeight,
+                        document.body.clientHeight, document.documentElement.clientHeight
+                    );
+                } else {
+                    wrapperHeight.value = scrollerWrapHtml.scrollHeight;
+                }
+            };
 
-                percent.value = (screenHeight.value / docHeight.value) * 100;
+            const countScreenHeight = () => {
+                if(props.body){
+                    screenHeight.value = document.documentElement.clientHeight;
+                } else {
+                    screenHeight.value = scrollerWrapHtml.clientHeight;
+                }
+            };
 
-                scrollElem = scroller.value as any;
-                scrollElem.style.height = percent.value + '%';
+            const countScrollerHeight = () => {
+                scrollerHeight.value = (screenHeight.value / wrapperHeight.value) * 100;
+                scrollerHtml.style.height = scrollerHeight.value + '%';
+            };
 
-                scrollerWrap = scrollerWrap.value as any;
-
+            const scrollHandeler = () => {
                 window.addEventListener('scroll', (e) => {
-                    
-                    let deltaY: number = window.scrollY - oldYVal.value;
-                    let newTop: number = (deltaY * screenHeight.value) / docHeight.value;
-                    
-                    console.log(window.scrollY, screenHeight.value, window)
-                    scrollElem.style.top = (newTop) + 'px';
+                    let newTop: number = (window.scrollY * screenHeight.value) / wrapperHeight.value;
+                    scrollerHtml.style.top = (newTop) + 'px';
                 });
             };
 
+
             onMounted(() => {
+                wrapperHtml      = wrapperHtmlRef.value as HTMLDivElement;
+                scrollerHtml     = scrollerHtmlRef.value as HTMLDivElement;
+                scrollerWrapHtml = scrollerWrapHtmlRef.value as HTMLDivElement;
 
                 setTimeout(() => {
-                    countData();
+                    countWrapperHeight();
+                    countScreenHeight();
+                    countScrollerHeight();
+                    scrollHandeler();
                 }, 0);
 
                 const resizeObserver = new ResizeObserver(entries => {
-                    countData();
+                    countWrapperHeight();
+                    countScreenHeight();
+                    countScrollerHeight();
+                    scrollHandeler();
                 });
 
-                resizeObserver.observe(document.body);
-            })
+                const elem = props.body ? document.body : wrapperHtml;
 
+                resizeObserver.observe(elem);
+            });
 
             return {
-                docHeight,
-
-                scrollerWrap,
-                scroller
+                wrapperHtmlRef,
+                scrollerHtmlRef,
+                scrollerWrapHtmlRef
             }
+
+            // let
+            //     oldYVal     : Ref<number> = ref(0),
+            //     scrollerWrap              = ref(null),
+            //     scroller                  = ref(null),
+            //     docHeight   : Ref<number> = ref(0),
+            //     screenHeight: Ref<number> = ref(0),
+            //     percent     : Ref<number> = ref(0);
+
+            // let
+            //     scrollWrap: HTMLDivElement,
+            //     scrollElem: HTMLDivElement;
+
+            // if(props.body){
+            //     screenHeight.value = document.documentElement.clientHeight;
+            // } else {
+            //     // screenHeight.value = document
+            // }
+
+            // const countData = () => {
+            //     docHeight.value = Math.max(
+            //         document.body.scrollHeight, document.documentElement.scrollHeight,
+            //         document.body.offsetHeight, document.documentElement.offsetHeight,
+            //         document.body.clientHeight, document.documentElement.clientHeight
+            //     );
+
+            //     percent.value = (screenHeight.value / docHeight.value) * 100;
+
+            //     scrollElem = scroller.value as any;
+            //     scrollElem.style.height = percent.value + '%';
+
+            //     scrollerWrap = scrollerWrap.value as any;
+
+            //     window.addEventListener('scroll', (e) => {
+                    
+            //         let deltaY: number = window.scrollY - oldYVal.value;
+            //         let newTop: number = (deltaY * screenHeight.value) / docHeight.value;
+                    
+            //         console.log(window.scrollY, screenHeight.value, window)
+            //         scrollElem.style.top = (newTop) + 'px';
+            //     });
+            // };
+
+            // onMounted(() => {
+
+            //     setTimeout(() => {
+            //         countData();
+            //     }, 0);
+
+            //     const resizeObserver = new ResizeObserver(entries => {
+            //         countData();
+            //     });
+
+            //     resizeObserver.observe(document.body);
+            // })
+
+
+            // return {
+            //     docHeight,
+
+            //     scrollerWrap,
+            //     scroller
+            // }
         }
     })
 </script>
@@ -102,9 +183,13 @@
     body {
         -ms-overflow-style: none;  /* IE and Edge */
         scrollbar-width: none;  /* Firefox */
-    } 
+    }
+
+    .scroll-main-wrapper{
+
+    }
     
-    .scroll-wrap{
+    .scroll-wrap-body{
         width: 10px;
         height: 100%;
         position: fixed;
@@ -113,7 +198,7 @@
         right: 0px;
     }
 
-    .scroller{
+    .scroller-body{
         position: fixed;
         right: 0px;
         width: 10px;
