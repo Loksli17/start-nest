d57ed766a545e047f84f1252bbefadf6fcfece7b
Loksli17 <ami0504@mail.ru>
2022-04-26 13:42:27 +1000

    new DB (dump here)

--- dump.sql
+++ dump.sql
@@ -88,7 +88,7 @@ CREATE TABLE `message` (
   KEY `fk_message_user1_idx` (`userId`),
   CONSTRAINT `fk_message_room1` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`),
   CONSTRAINT `fk_message_user1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,6 +97,7 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,'message 123',3,1),(2,'message 123',1,1),(3,'message text',2,1),(4,'new message',3,1);
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,8 +111,12 @@ DROP TABLE IF EXISTS `room`;
 CREATE TABLE `room` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `userId` int NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_room_user1_idx` (`userId`),
+  CONSTRAINT `fk_room_user1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,6 +125,7 @@ CREATE TABLE `room` (
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,'New name',1,'default.png'),(2,'Another room',1,'default.png'),(3,'Room',2,'default.png'),(4,'Big room',3,'default.png');
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,7 +145,7 @@ CREATE TABLE `room_has_user` (
   KEY `fk_room_has_user_room1_idx` (`roomId`),
   CONSTRAINT `fk_room_has_user_room1` FOREIGN KEY (`roomId`) REFERENCES `room` (`id`),
   CONSTRAINT `fk_room_has_user_user1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +154,7 @@ CREATE TABLE `room_has_user` (
 
 LOCK TABLES `room_has_user` WRITE;
 /*!40000 ALTER TABLE `room_has_user` DISABLE KEYS */;
+INSERT INTO `room_has_user` VALUES (1,1,1),(2,2,1),(3,1,2),(5,3,2),(6,3,1),(7,4,3);
 /*!40000 ALTER TABLE `room_has_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -191,7 +198,7 @@ CREATE TABLE `task` (
   PRIMARY KEY (`id`),
   KEY `fk_task_taskType_idx` (`taskTypeId`),
   CONSTRAINT `fk_task_taskType` FOREIGN KEY (`taskTypeId`) REFERENCES `tasktype` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -200,7 +207,7 @@ CREATE TABLE `task` (
 
 LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` VALUES (38,'2022-02-14','05:51:31','Teeeext',2),(44,'2022-04-21','12:08:46','Teeeext (111)',4),(48,'2022-02-15','12:28:28','test text (2000 test)',3),(56,'2022-02-16','12:45:08','HMMM',1),(57,'2022-04-22','09:52:08','NEW NAAAAME',4),(59,'2022-02-15','01:17:03','highload test 5!!!!!',1),(83,'2022-02-15','06:49:34','for scroll',1),(86,'2022-02-15','06:49:43','for scroll',1),(88,'2022-02-15','08:06:50','Text..',1),(98,'2022-02-17','08:42:32','Text..',1),(99,'2022-02-17','08:42:34','Text..',1),(100,'2022-02-17','08:42:36','Text..',1),(101,'2022-02-17','08:42:38','Text..',1),(102,'2022-02-17','08:43:03','Text..',1),(103,'2022-02-17','08:43:12','Text..',1),(106,'2022-02-17','08:50:51','Text..',1),(112,'2020-02-29','08:50:51','Text..',1),(115,'2022-04-21','12:09:02','Teeeext (111)',3),(118,'2022-04-22','02:32:09','Text..',2),(119,'2022-04-22','02:32:13','Text..',2),(120,'2022-04-22','02:32:17','Text..',3);
+INSERT INTO `task` VALUES (38,'2022-02-14','05:51:31','Teeeext',2),(44,'2022-04-21','12:08:46','Teeeext (111)',4),(48,'2022-02-15','12:28:28','test text (2000 test)',3),(57,'2022-04-22','09:52:08','NEW NAAAAME',4),(59,'2022-02-15','01:17:03','highload test 5!!!!!',1),(83,'2022-02-15','06:49:34','for scroll',1),(86,'2022-02-15','06:49:43','for scroll',1),(88,'2022-02-15','08:06:50','Text..',1),(98,'2022-02-17','08:42:32','Text..',1),(99,'2022-02-17','08:42:34','Text..',1),(100,'2022-02-17','08:42:36','Text..',1),(101,'2022-02-17','08:42:38','Text..',1),(112,'2020-02-29','08:50:51','Text..',1),(115,'2022-04-21','12:09:02','Teeeext (111)',3),(118,'2022-04-22','02:32:09','Text..',2),(119,'2022-04-22','02:32:13','Text..',2),(120,'2022-04-22','02:32:17','Text..',3),(121,'2022-04-23','05:56:14','Text..',1),(122,'2022-04-25','11:53:57','Text..',2);
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,6 +233,35 @@ LOCK TABLES `tasktype` WRITE;
 /*!40000 ALTER TABLE `tasktype` DISABLE KEYS */;
 INSERT INTO `tasktype` VALUES (1,'Important'),(2,'Urgent'),(3,'Not-Important'),(4,'Not-Urgent');
 /*!40000 ALTER TABLE `tasktype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `token` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) NOT NULL,
+  `client` varchar(400) NOT NULL,
+  `userId` int NOT NULL,
+  `expiredIn` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_token_user1_idx` (`userId`),
+  CONSTRAINT `fk_token_user1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `token`
+--
+
+LOCK TABLES `token` WRITE;
+/*!40000 ALTER TABLE `token` DISABLE KEYS */;
+INSERT INTO `token` VALUES (1,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6Imxva3NsaSIsImlkIjoxLCJpYXQiOjE2NTA5NDQyNjEsImV4cCI6MTY1MTAzMDY2MX0.j3YooGd8PqRSnhz7xifj-4-O3ZsGTfW6yKpJBoq2-W0','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',1,2940000),(2,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6Imxva3NsaSIsImlkIjoxLCJpYXQiOjE2NTA4ODkxNjMsImV4cCI6MTY1MDk3NTU2M30.Eb0rY-KxrGNlktBROMZUjCd7GS7iFPMal7_zKQHkomc','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.72',1,3600000);
+/*!40000 ALTER TABLE `token` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -262,4 +298,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-23 12:26:08
+-- Dump completed on 2022-04-26 13:41:52
