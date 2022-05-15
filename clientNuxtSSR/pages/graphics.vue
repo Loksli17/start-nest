1e0c8b98cb72433c43ae89c6a87cb49ee2f9c16a
Alexander <sansanichick@gmail.com>
2022-05-15 15:00:25 +1000

    nuxt pixijs tests, formkit update

---
+++ clientNuxtSSR/pages/graphics.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+    import * as PIXI from "pixi.js";
+
+    const container = ref(null as null | HTMLDivElement);
+
+    onMounted(() => {
+        const app = new PIXI.Application({ width: 640, height: 480 });
+        
+        (container.value!).appendChild(app.view);
+
+        const rect = new PIXI.Graphics();
+        rect.interactive = true;
+        rect.buttonMode = true;
+        
+        rect.beginFill(0xFF0000);
+        rect.drawRect(50, 50, 100, 100);
+
+        
+
+        rect.on("pointerover", e => {
+            rect.tint = 0x666666;
+        });
+
+        rect.on("pointerout", e => {
+            rect.tint = 0xFFFFFF;
+        });
+
+        
+        // ! PIXI.js docs say to use 'this' in here,
+        // ! yet TS complains about 'this' being weird
+        rect
+            .on("pointerdown", function (event) {
+                this.data = event.data;
+                this.dragging = true;
+            })
+            .on("pointermove", function () {
+                if (this.dragging) {
+                    const newPos = this.data.getLocalPosition(this.parent);
+                    this.x = newPos.x - 100;
+                    this.y = newPos.y - 100;
+                }
+            })
+            .on("pointerup", function () {
+                this.dragging = false;
+                this.data = null;
+            })
+            .on("pointerupoutside", function () {
+                this.alpha = 1;
+                this.dragging = false;
+                this.data = null;
+            });
+
+        app.stage.addChild(rect);
+    });
+
+    
+</script>
+
+<template>
+    <div>
+        <div ref="container"></div>
+        <CustomLink to="/">home</CustomLink>
+    </div>
+</template>
