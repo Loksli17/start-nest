ae3d68589fa5ffd57a8a5d14e7f00bed72478a93
Alexander <sansanichick@gmail.com>
2022-05-20 00:37:43 +1000

    nuxt fix

--- client-nuxt/app.vue
+++ client-nuxt/app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+    useHead({
+        title: "Index",
+        titleTemplate: "Cool site | %s",
+        viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+        charset: "utf-8",
+        meta: [
+            { name: "description",  content: "Cool site description" },
+            { name: "color-scheme", content: "dark light" }
+        ]
+    });
+</script>
+
 <template>
     <div>
         <NuxtLayout>
