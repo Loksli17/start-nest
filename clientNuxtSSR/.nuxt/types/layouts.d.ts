2ea273a031ad86c9a5210b31b0c4132c525ab753
Loksli17 <ami0504@mail.ru>
2022-05-20 13:39:05 +1000

    fix kaka

--- clientNuxtSSR/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "custom"
-declare module "C:/Users/ASUS/Desktop/develop/back-end/start-nest/clientnuxtssr/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- clientNuxtSSR/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "C:/Users/ASUS/Desktop/develop/back-end/start-nest/clientnuxtssr/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
