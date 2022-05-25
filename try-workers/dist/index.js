354152205f0382cc114220c150533f51a53372d6
Loksli17 <ami0504@mail.ru>
2022-05-25 23:53:07 +1000

    emulate server

--- try-workers/dist/index.js
+++ try-workers/dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const worker_threads_1 = require("worker_threads");
-const main = () => {
+//@ts-ignore
+const readline_promise_1 = __importDefault(require("readline-promise"));
+const workerFactory = () => {
     const worker = new worker_threads_1.Worker("./dist/worker.js");
     worker.on("message", (value) => {
         console.log(`[message]: ${value}`);
@@ -9,7 +14,32 @@ const main = () => {
     worker.on("error", (err) => {
         console.error(err);
     });
-    // const interval: number = setInterval(() => {
-    // }, 1000);
+    return worker;
+};
+const workers = [];
+const main = async () => {
+    const readPromise = readline_promise_1.default.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: true
+    });
+    while (true) {
+        const value = await readPromise.questionAsync('Foo?');
+        console.log(value);
+        // readLine.question("1 - create new worker, 2 - show exists", (answer: string) => {
+        //     console.log(answer);
+        //     switch(answer) {
+        //         case "1":
+        //             const worker: Worker = workerFactory();
+        //             workers.push(worker);
+        //             break;
+        //         case "2":
+        //             console.log(workers);
+        //             break;
+        //         default:
+        //             break; 
+        //     }
+        // });
+    }
 };
 main();
--- try-workers/src/index.ts
+++ try-workers/src/index.ts
@@ -1,8 +1,15 @@
 import { Worker } from "worker_threads";
+//@ts-ignore
+import readline from 'readline-promise';
 
 
-const main = () => {
-    
+interface workerData {
+    id: number;
+}
+
+
+const workerFactory = (data: workerData): Worker => {
+
     const worker: Worker = new Worker("./dist/worker.js");
 
     worker.on("message", (value: any) => {
@@ -12,6 +19,42 @@ const main = () => {
     worker.on("error", (err: Error) => {
         console.error(err);
     });
+
+    return worker;
+}
+
+const workers: Array<Worker> = [];
+
+
+const main = async () => {
+
+    const readPromise = readline.createInterface({
+        input   : process.stdin,
+        output  : process.stdout,
+        terminal: true
+    });
+
+
+    while(true) {
+
+        const answer: string = await readPromise.questionAsync("1 - create new worker, 2 - show exists");
+
+        switch(answer) {
+
+            case "1":
+                const worker: Worker = workerFactory({id: workers.length + 1});
+                workers.push(worker);
+                break;
+
+            case "2":
+                console.log(workers);
+                break;
+
+            default:
+                break; 
+        }
+    }
 };
+
 
 main();
