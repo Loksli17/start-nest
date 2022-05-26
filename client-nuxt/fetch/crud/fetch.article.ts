adb9c84616d54af2b5ab05703ab015d8095f4c11
Alexander <sansanichick@gmail.com>
2022-05-26 15:57:56 +1000

    nuxt axios plugin

--- client-nuxt/fetch/crud/fetch.article.ts
+++ client-nuxt/fetch/crud/fetch.article.ts
@@ -1,10 +1,12 @@
-import axios, { AxiosResponse } from "axios";
+import  { AxiosResponse } from "axios";
+
 
 export default class ArticleFetch {
-
+    
     public async searchArticle(data: string): Promise<AxiosResponse> {
-
-        const response: AxiosResponse = await axios.post("http://127.0.0.1:3000/article/search", {
+        const { $axios } = useNuxtApp();
+        
+        const response: AxiosResponse = await $axios.post("http://127.0.0.1:3000/article/search", {
             searchData: data,
         });
 
